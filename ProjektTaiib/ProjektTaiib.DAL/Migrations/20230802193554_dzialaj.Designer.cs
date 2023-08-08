a1ef11f7941ee7d6053ac3be25f0399e8cbada6c
JakubJucha <jakujuc054@student.polsl.pl>
2023-08-08 20:57:55 +0200

    Połączenie front + back próba 1

--- ProjektTaiib/ProjektTaiib.DAL/Migrations/20230802193554_dzialaj.Designer.cs
+++ ProjektTaiib/ProjektTaiib.DAL/Migrations/20230802193554_dzialaj.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjektTaiib.DAL;
 
@@ -11,9 +12,11 @@ using ProjektTaiib.DAL;
 namespace ProjektTaiib.DAL.Migrations
 {
     [DbContext(typeof(ProjektTaiibDbContext))]
-    partial class ProjektTaiibDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230802193554_dzialaj")]
+    partial class dzialaj
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
