310ce7355db46f9d09af0b6b9d0e4d21f0e2bfde
JakubJucha <jakujuc054@student.polsl.pl>
2023-08-23 21:56:54 +0200

    ?

--- ProjektTaiib/ProjektTaiibWeb/DTO/RegisterRequest.cs
+++ ProjektTaiib/ProjektTaiibWeb/DTO/RegisterRequest.cs
@@ -2,6 +2,6 @@
 {
     public record class RegisterRequest(string Email, string LoginRegister,string PasswordRegister,
         string? Name,string? Surname, string? PhoneNumber, string? ZIPCode,string? Country, string? City,
-        string? Street, int HouseNumber, int? LocalNumber, string? Payment, string? AdditionalInformation);
+        string? Street, int? HouseNumber, int? LocalNumber, string? Payment, string? AdditionalInformation);
    
 }
